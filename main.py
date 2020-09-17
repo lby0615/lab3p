795bdf6e387b94d5e13809588e2685ef0616a9dd
lby0615 <bjl5716@psu.edu>
2020-09-17 15:02:21 +0000

    add info&make corection

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 # Author: Boyuan Lai bjl5716@psu.edu
-# Collaborator:
-# Collaborator:
-# Collaborator:
-# Section: 1
-# Breakout: 12
+# Collaborator: Alex Podlucky app5775@psu.edu
+# Collaborator: Ilysa Sanchez-Perez ims5280@psu.edu
+# Collaborator: Hetvi Devani hdd5060@psu.edu
+# Section: 12
+# Breakout: 10
 def sum_n(n):
-  if n <= 1:
-    return 1
+  if n == 0:
+    return n
   else:
     return n+sum_n(n-1)
 
 def print_n(s, n):
-  return s*n
+  if n == 0:
+    return
+  else:
+    print(s)
+    print_n(s,n-1)
+    return
 
 def run():
   num = int(input("Enter an int: "))
   print (f"sum is: {sum_n(num)}")
   letter = input("Enter a string:")
-  print (print_n(letter, num))
+  print_n(letter, num)
   
-
 if __name__ == "__main__":
   run()
